261f2a7f1c35a155df0b2526612c0de771153c27
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2021-04-26 21:26:06 +0200

    Refactor GPU code

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@ xas99.py -R -S -L zaxxon.lst -w src/zaxxon.a99 -o zaxxon.obj
 
 xas99.py -R -i -w src/zaxxon.a99 -o bin/ZAXXON
 
-java -jar tools/ea5tocart.jar bin/ZAXXON "ZAXXON" > make.log
+java -jar tools/ea5tocart.jar bin/ZAXXON "ZAXXON DEMO" > make.log
 
 tools\pad.exe bin\map1.bin bin\map1-8k.bin 8192
 
