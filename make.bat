5793874c9ee839832b979e1b60931dd90f655451
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2021-07-16 20:49:04 +0200

    Rename to zqx-one

--- make.bat
+++ make.bat
@@ -1,9 +1,9 @@
-xas99.py -R -S -L zaxxon.lst -w src/zaxxon.a99 -o zaxxon.obj
+xas99.py -R -S -L zqx-one.lst -w src/zqx-one.a99 -o zqx-one.obj
 @IF %ERRORLEVEL% NEQ 0 GOTO :end
 
-xas99.py -R -i -w src/zaxxon.a99 -o bin/ZAXXON
+xas99.py -R -i -w src/zqx-one.a99 -o bin/ZQX-ONE
 
-java -jar tools/ea5tocart.jar bin/ZAXXON "ZAXXON DEMO" > make.log
+java -jar tools/ea5tocart.jar bin/ZQX-ONE "ZXQ-ONE" > make.log
 
 tools\pad.exe bin\map0.bin bin\map0-8k.bin 8192
 tools\pad.exe bin\map1.bin bin\map1-8k.bin 8192
@@ -19,7 +19,7 @@ xas99.py -b -w patterns/char-patterns1-3.a99 -o bin/char-patterns1-3.bin
 xas99.py -b -w patterns/char-patterns2-1.a99 -o bin/char-patterns2-1.bin
 xas99.py -b -w patterns/char-patterns2-2.a99 -o bin/char-patterns2-2.bin
 
-copy /b bin\ZAXXON8.bin + ^
+copy /b bin\ZQX-ONE8.bin + ^
     bin\map1-8k.bin + ^
     bin\map2-8k.bin + ^
     bin\map0-8k.bin + ^
@@ -32,8 +32,8 @@ copy /b bin\ZAXXON8.bin + ^
     bin\empty-1k.bin + bin\char-patterns0-1.bin + ^
     bin\empty-2k.bin + bin\sprite-patterns-0.bin + ^
     bin\empty-2k.bin + bin\sprite-patterns-2.bin ^
-    .\zaxxon8.bin
+    .\zxq-one-8.bin
 
-java -jar tools/CopyHeader.jar zaxxon8.bin 60 4 5 6
+java -jar tools/CopyHeader.jar zxq-one-8.bin 60 4 5 6
 
 :end
