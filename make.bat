5e334f29ec02b8a71c1cd2dbd5fae9e92e899efc
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2021-12-30 20:57:59 +0100

    Update for latest xas99

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
-xas99.py -R -S -L zqx-one.lst -w src/zqx-one.a99 -o zqx-one.obj
+xas99.py -R -S -L zqx-one.lst -q -18 src/zqx-one.a99 -o zqx-one.obj
 @IF %ERRORLEVEL% NEQ 0 GOTO :end
 
-xas99.py -R -i -w src/zqx-one.a99 -o bin/ZQX-ONE
+xas99.py -R -i -q -18 src/zqx-one.a99 -o bin/ZQX-ONE
 
 java -jar tools/ea5tocart.jar bin/ZQX-ONE "ZQX-ONE" > make.log
 
@@ -9,15 +9,15 @@ tools\pad.exe bin\map0.bin bin\map0-8k.bin 8192
 tools\pad.exe bin\map1.bin bin\map1-8k.bin 8192
 tools\pad.exe bin\map2.bin bin\map2-8k.bin 8192
 
-xas99.py -b -w patterns/sprite-patterns-0.a99 -o bin/sprite-patterns-0.bin
-xas99.py -b -w patterns/sprite-patterns-1.a99 -o bin/sprite-patterns-1.bin
-xas99.py -b -w patterns/sprite-patterns-2.a99 -o bin/sprite-patterns-2.bin
-xas99.py -b -w patterns/char-patterns0-1.a99 -o bin/char-patterns0-1.bin
-xas99.py -b -w patterns/char-patterns1-1.a99 -o bin/char-patterns1-1.bin
-xas99.py -b -w patterns/char-patterns1-2.a99 -o bin/char-patterns1-2.bin
-xas99.py -b -w patterns/char-patterns1-3.a99 -o bin/char-patterns1-3.bin
-xas99.py -b -w patterns/char-patterns2-1.a99 -o bin/char-patterns2-1.bin
-xas99.py -b -w patterns/char-patterns2-2.a99 -o bin/char-patterns2-2.bin
+xas99.py -b -q patterns/sprite-patterns-0.a99 -o bin/sprite-patterns-0.bin
+xas99.py -b -q patterns/sprite-patterns-1.a99 -o bin/sprite-patterns-1.bin
+xas99.py -b -q patterns/sprite-patterns-2.a99 -o bin/sprite-patterns-2.bin
+xas99.py -b -q patterns/char-patterns0-1.a99 -o bin/char-patterns0-1.bin
+xas99.py -b -q patterns/char-patterns1-1.a99 -o bin/char-patterns1-1.bin
+xas99.py -b -q patterns/char-patterns1-2.a99 -o bin/char-patterns1-2.bin
+xas99.py -b -q patterns/char-patterns1-3.a99 -o bin/char-patterns1-3.bin
+xas99.py -b -q patterns/char-patterns2-1.a99 -o bin/char-patterns2-1.bin
+xas99.py -b -q patterns/char-patterns2-2.a99 -o bin/char-patterns2-2.bin
 
 copy /b bin\ZQX-ONE8.bin + ^
     bin\map1-8k.bin + ^
